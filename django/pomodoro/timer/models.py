6f315fea9ea32a245caae74508ceeba2c3cb6a5e
Scott Hutchinson <scott@hutchinson.engineering>
2022-05-29 14:32:28 +1000

    Removed django folder/project

--- django/pomodoro/timer/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Timer(models.Model):
-    start_time = models.DateTimeField(null=False, blank=False)
-    end_time = models.DateTimeField(null=True, blank=True)
